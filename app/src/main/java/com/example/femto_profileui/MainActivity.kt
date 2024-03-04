3ed26ab18fd4178c22b09844edb794ff6dc4fea8
Ultron03 <ujjwaltiwari232@gmail.com>
2024-03-05 02:06:37 +0530

    My Membership Page Added

--- app/src/main/java/com/example/femto_profileui/MainActivity.kt
+++ app/src/main/java/com/example/femto_profileui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var editProfile:CardView
+    private lateinit var myMembership:CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,8 +16,12 @@ class MainActivity : AppCompatActivity() {
         editProfile.setOnClickListener {
             startActivity(Intent(this,EditProfileActivity::class.java))
         }
+        myMembership.setOnClickListener {
+            startActivity(Intent(this,MemberShipActivity::class.java))
+        }
     }
     private fun init(){
         editProfile = findViewById(R.id.edit_profile)
+        myMembership = findViewById(R.id.myMembership_button)
     }
 }
